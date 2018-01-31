9b68d90613af79480b95b8a6f1ca5816171bb2d6
Link <1316341442@qq.com>
2018-01-31 09:18:35 +0000

    Create update.js

---
+++ update/update.js
@@ -0,0 +1,6 @@
+document.getElementById("new").innerHTML="1.2.0";
+x=document.getElementById("update");
+  var up=x.innerHTML;
+  if(up<1.2){
+  document.getElementById("cut").innerHTML="我们建议您根据需要，更新或者是保留原有版本。";
+  }
