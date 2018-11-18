dbd380a4557436708aaff7b87b7f4ff0fa83c7ae
Link <1316341442@qq.com>
2018-11-18 14:57:42 +0800

    Update update.js

--- update/update.js
+++ update/update.js
@@ -2,5 +2,8 @@ document.getElementById("new").innerHTML="1.2.0";
 x=document.getElementById("update");
   var up=x.innerHTML;
   if(up<1.2){
-  document.getElementById("cut").innerHTML="我们建议您根据需要，更新或者是保留原有版本。";
+  document.getElementById("cut").innerHTML="我们建议您根据需要，更新或者是保留原有版本";
+  }
+  else{
+  document.getElementById("cut").innerHTML="已经是最新版本";
   }
