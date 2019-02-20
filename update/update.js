2e4dd35e851d9556f0f98e3136b809b689f2838f
Link <1316341442@qq.com>
2019-02-20 16:31:33 +0800

    Update update.js

--- update/update.js
+++ update/update.js
@@ -1,7 +1,7 @@
-document.getElementById("new").innerHTML="1.0.0 Beta";
+document.getElementById("new").innerHTML="1.1.0 Beta";
 x=document.getElementById("update");
   var up=x.innerHTML;
-  if(up<1.0){
+  if(up<1.1){
   document.getElementById("cut").innerHTML="有新更新可用<p>更新内容:<p>新增置顶功能<p>我们建议您根据需要，更新或是保留原有版本";
   }
   else{
