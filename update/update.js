519297d3ecf8cd924021b4abcd4e8d4fcae2d0ed
Link <1316341442@qq.com>
2019-07-20 22:16:26 +0800

    Update update.js

--- update/update.js
+++ update/update.js
@@ -2,7 +2,7 @@ document.getElementById("new").innerHTML="1.1.0 正式版";
 x=document.getElementById("update");
   var up=x.innerHTML;
   if(up<1.1){
-  document.getElementById("cut").innerHTML="有新更新可用<p>更新内容:<h1>1.新增置顶功能</h1><h1>2.新增MathJax功能</h1>我们建议您根据需要，更新或是保留原有版本";
+  document.getElementById("cut").innerHTML="有新更新可用<p>更新内容:<h1>1.新增置顶功能</h1><h1>2.新增MathJax功能</h1><h1>3.修复Chrome地址栏主题色不同步问题</h1>我们建议您根据需要，更新或是保留原有版本";
   }
   else{
   document.getElementById("cut").innerHTML="已经是最新版本";
