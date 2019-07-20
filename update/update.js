1e0857b358b0b558c40d91096d6598b091b3bbb4
Link <1316341442@qq.com>
2019-07-20 17:55:02 +0800

    Update update.js

--- update/update.js
+++ update/update.js
@@ -2,7 +2,14 @@ document.getElementById("new").innerHTML="1.1.0 正式版";
 x=document.getElementById("update");
   var up=x.innerHTML;
   if(up<1.1){
-  document.getElementById("cut").innerHTML="有新更新可用<p>更新内容:<h1>1.新增置顶功能</h1><h1>2.新增MathJax功能</h1><h1>3.新增Dashboard</h1>我们建议您根据需要，更新或是保留原有版本";
+  document.getElementById("cut").innerHTML="<i style="display:block;
+	background:#f00;
+	border-radius:50%;
+	width:5px;
+	height: 5px;
+	top:45%;
+	right:0px;
+	position:absolute;"></i>有新更新可用<p>更新内容:<h1>1.新增置顶功能</h1><h1>2.新增MathJax功能</h1><h1>3.新增Dashboard</h1>我们建议您根据需要，更新或是保留原有版本";
   }
   else{
   document.getElementById("cut").innerHTML="已经是最新版本";
